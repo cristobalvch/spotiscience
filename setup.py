99d0de5892b67f379d1c7f15c7c023928fb66a6d
cristobal veas <cristobal.veas.ch@gmail.com>
2021-07-05 18:11:50 -0400

    new configurations in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ import codecs
 import os
 
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
 
 VERSION = '0.0.1'
 DESCRIPTION = 'A Library for Data Scientist for extracting and modelling music data'
-LONG_DESCRIPTION = 'A package that allows to obtain and to model information of songs on Spotify for Research Data Science Purposes'
 
 # Setting up
 setup(
@@ -18,9 +22,8 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=['spotipy', 'lyricsgenius','pandas','numpy','scikit-learn','spacy',
-                      'en_core_web_lg @ https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-3.0.0/en_core_web_lg-3.0.0.tar.gz',
-                       'es_core_news_lg @ https://github.com/explosion/spacy-models/releases/download/es_core_news_lg-3.0.0/es_core_news_lg-3.0.0.tar.gz'],
+    install_requires=['spotipy', 'lyricsgenius','pandas','numpy','scikit-learn','spacy','en_core_web_lg@https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-3.0.0/en_core_web_lg-3.0.0.tar.gz#egg=en_core_web_lg',
+                      'es_core_news_lg@https://github.com/explosion/spacy-models/releases/download/es_core_news_lg-3.0.0/es_core_news_lg-3.0.0.tar.gz#egg=es_core_news_lg'],
     keywords=['python', 'data', 'data science', 'music', 'spotify', 'machine learning','topicmodelling','natural language processing'],
     classifiers=[
         "Development Status :: 1 - Planning",
