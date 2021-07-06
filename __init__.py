0d73912962e5a4db9f68c65815f03ffc86d2ee5d
cristobal veas <cristobal.veas.ch@gmail.com>
2021-07-06 08:53:00 -0400

    first commit

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from spotiscience.spotiscience.downloader import SpotiScienceDownloader
-from spotiscience.spotiscience.prediction import SpotiSciencePredicter
-from spotiscience.spotiscience.credentials import CREDENTIALS
---
+++ spotiscience/__init__.py
@@ -0,0 +1,3 @@
+from spotiscience.downloader import SpotiScienceDownloader
+from spotiscience.prediction import SpotiSciencePredicter
+from spotiscience.credentials import CREDENTIALS
